0a78e097853c56ef3bb84f4347f23d8cf42aac43
adwitafernanda <adwitafernanda4@gmail.com>
2023-05-24 15:22:59 +0700

    membuat class pelajar

--- ParadigmaOOP/ParadigmaOOP.cpp
+++ ParadigmaOOP/ParadigmaOOP.cpp
@@ -17,4 +17,22 @@ public:
 	int jumlah(int a, int b) {
 		return a + b;
 	}
+
+};
+
+class pelajar : public orang {
+public:
+	string sekolah;
+
+	pelajar(string pNama, string pSekolah) :
+		orang(pNama),
+		sekolah(pSekolah) {
+		cout << "Pelajar dibuat\n" << endl;
+	}
+	~pelajar() {
+		cout << "Pelajar dihapus\n" << endl;
+	}
+	string perkenalan() {
+		return "Hallo, nama saya" + nama + "dari sekolah" + sekolah + "\n\n";
+	}
 };
